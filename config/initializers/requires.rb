6e84d8878e700a56df0313cb11a3aa86c78a74af
Luismi Cavalle <luismi@lmcavalle.com>
2009-08-08 11:38:36 +0200

    Added required gems to environment.rb. Making sure "rake gems:install" works

--- config/initializers/requires.rb
+++ config/initializers/requires.rb
@@ -1,9 +1,5 @@
 require 'utils'
-require 'paperclip_validations_extended'
-require 'will_paginate'
-require 'pdf/writer'
 require 'pdf/simpletable'
-require 'bluecloth'
 
 # extensions
 require 'pdfwritter_extensions'
