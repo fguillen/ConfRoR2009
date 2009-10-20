55b9703bc6c7d5a87dc66ec89f8d6a580cc3c7c7
Luismi Cavalle <luismi@lmcavalle.com>
2009-10-21 01:31:05 +0200

    Registration waiting to be opened

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    redirect_to new_cart_path if current_user
-    redirect_to new_session_path
+    # redirect_to new_cart_path if current_user
+    # redirect_to new_session_path 
+    redirect_to papers_path
   end
 end
